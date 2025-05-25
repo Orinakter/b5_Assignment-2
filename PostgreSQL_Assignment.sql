f584228c91c916edf7873edb10b60a977cb6fa55
Orinakter <orin.akter9013@gmail.com>
2025-05-25 23:01:50 +0600

    solving problem 2,3

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -79,4 +79,18 @@ SELECT * FROM sightings;
 
 INSERT INTO rangers (name, region) VALUES ('Derek Fox', 'Coastal Plains');
 
+-- Problem-2: Count unique species ever sighted.
+
+SELECT COUNT(DISTINCT species_id) AS unique_species_count FROM sightings;
+
+-- Problem-3:  Find all sightings where the location includes "Pass".
+
+SELECT * FROM sightings
+WHERE location ILIKE '%Pass%';
+
+
+
+
+
+
 
