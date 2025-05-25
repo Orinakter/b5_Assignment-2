bd2efe2d2c5a79d1279f473eb9d09d8832f2f25f
Orinakter <orin.akter9013@gmail.com>
2025-05-25 23:56:04 +0600

    Sloving Problem 5,6,7

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -95,6 +95,29 @@ FROM rangers r
 LEFT JOIN sightings s ON r.ranger_id = s.ranger_id
 GROUP BY r.name;
 
+-- Problem-5 : List species that have never been sighted.
+
+SELECT common_name FROM species
+WHERE species_id NOT IN (
+    SELECT DISTINCT species_id FROM sightings
+);
+
+-- Problem-6 :  Show the most recent 2 sightings.
+
+SELECT sp.common_name, s.sighting_time, r.name
+FROM sightings s
+JOIN species sp ON s.species_id = sp.species_id
+JOIN rangers r ON s.ranger_id = r.ranger_id
+ORDER BY s.sighting_time DESC
+LIMIT 2;
+
+
+-- Problem-7 : Update all species discovered before year 1800 to have status 'Historic'.
+
+UPDATE species
+SET conservation_status = 'Historic'
+WHERE discovery_date < '1800-01-01';
+
 
 
 
